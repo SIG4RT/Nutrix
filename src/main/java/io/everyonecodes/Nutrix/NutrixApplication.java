a3bd4188ff65655ae69ef57aacf033dbb0005ba4
SIG4RT <130319695+SIG4RT@users.noreply.github.com>
2023-11-12 19:42:20 +0100

    initial commit - spring initializr + dependencies (´jpa, postgres, lombok, thymleaf, web, devTools)

---
+++ src/main/java/io/everyonecodes/Nutrix/NutrixApplication.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.Nutrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/everyonecodes/Nutrix/NutrixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.Nutrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
